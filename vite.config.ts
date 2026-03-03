0baf8c7e068366a9a35ff8a19cb35a0f1f198497
mksga <mks.g.a.ua@gmail.com>
2026-03-03 20:59:31 +0200

    ispravil

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // Укажите имя вашего репозитория вместо 'YOUR_REPO_NAME'
-      // Если это основной сайт (username.github.io), оставьте '/'
+      // Это говорит Vite, что сайт лежит в папке /GSF/
       base: '/GSF/', 
       server: {
         port: 3000,
@@ -14,8 +13,9 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-  'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
-},
+        // Пробрасываем ключ, который вы назвали VITE_GEMINI_API_KEY
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+      },
       resolve: {
         alias: {
           '@': path.resolve('.'),
